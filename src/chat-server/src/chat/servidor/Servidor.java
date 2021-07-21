09eadbe86bd83164614ad6129d2c784c493bc1ed
Lissandro Perossi Cunha <lcunha@faepa.br>
2021-07-21 11:21:32 -0300

    finalizado projetinho de chat da algaworks

--- src/chat-server/src/chat/servidor/Servidor.java
+++ src/chat-server/src/chat/servidor/Servidor.java
@@ -1,5 +1,46 @@
 package chat.servidor;
 
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import chat.servidor.thread.RecebeMensagemCliente;
+
 public class Servidor {
+
+	private List<RecebeMensagemCliente> clientes = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Servidor servidor = new Servidor();
+		servidor.aguardarConexoes(servidor);
+	}
+
+	private void aguardarConexoes(Servidor servidor) {
+		try (ServerSocket server = new ServerSocket(3333)) {
+
+			while (true) {
+				System.out.println("Aguardando conexões...");
+				Socket socket = server.accept();
+
+				RecebeMensagemCliente recebeMensagemCliente = new RecebeMensagemCliente(socket, this);
+				new Thread(recebeMensagemCliente).start();
+
+				servidor.clientes.add(recebeMensagemCliente);
+				System.out.println("Novo cliente conectado.");
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void enviarMensagensClientes(String mensagem) {
+		for (RecebeMensagemCliente cliente : this.clientes) {
+			cliente.enviarMensagem(mensagem);
+		}
+	}
 
 }
