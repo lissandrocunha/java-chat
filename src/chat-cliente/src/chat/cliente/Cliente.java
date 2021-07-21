df8a8409db15382692bce0f03cb844b197846a68
Lissandro Perossi Cunha <lcunha@faepa.br>
2021-07-21 10:47:02 -0300

    finalizado rotinas do cliente

--- src/chat-cliente/src/chat/cliente/Cliente.java
+++ src/chat-cliente/src/chat/cliente/Cliente.java
@@ -1,19 +1,47 @@
 package chat.cliente;
 
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 import chat.cliente.gui.JanelaGui;
+import chat.cliente.thread.RecebeMensagemServidor;
 
 public class Cliente extends JanelaGui {
 
+	private Socket socket;
+
+	public static void main(String args[]) {
+		new Cliente();
+	}
+
 	@Override
 	protected boolean conectar() {
-		// TODO Auto-generated method stub
-		return false;
+		System.out.println("Conectando no servidor...");
+		try {
+			this.socket = new Socket("127.0.0.1", 3333);
+			RecebeMensagemServidor recebeMensagemServidor = new RecebeMensagemServidor(this.socket, this);
+			new Thread(recebeMensagemServidor).start();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 
 	@Override
 	protected void sendMessage(String mensagem) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(String.format("Envia a mensagem via socket para o servidor - %s", mensagem));
+
+		try {
+			OutputStream os = this.socket.getOutputStream();
+			DataOutput dos = new DataOutputStream(os);
+			dos.writeUTF(mensagem);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
